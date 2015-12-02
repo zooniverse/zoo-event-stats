d9693450b3a6765e3fa5a7e073ac44cd71767b90
Campbell Allen <campbell.allen@gmail.com>
2015-12-02 14:38:30 +0000

    cleanup

--- lib/output/elasticsearch_writer.rb
+++ lib/output/elasticsearch_writer.rb
@@ -13,18 +13,6 @@ module Stats
         client.cluster.health
       end
 
-      # def index
-      #   client.index index: config[:index], type: 'my-document', id: 1, body: { title: 'Test' }
-      # end
-
-      # def refresh_index
-      #   client.indices.refresh index: config[:index]
-      # end
-
-      # def search
-      #   client.search index: config["index"], body: { query: { match: { title: 'test' } } }
-      # end
-
       def get(id)
         client.get doc_defaults.merge(id: id)
       end
