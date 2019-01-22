36a5983ef023c2b08bf9952d08ae98b6998bfab4
Campbell Allen <campbell.allen@gmail.com>
2019-01-22 12:47:52 +0000

    switch to swarm logging
    ensure logs go to std out & err for Cloudwatch logging

--- config/puma.rb
+++ config/puma.rb
@@ -2,9 +2,4 @@ dev_env = 'development'
 env = ENV['RACK_ENV'] || dev_env
 port = env == dev_env ? 3000 : 80
 environment env
-
-if env == "production" || env == "staging"
-  stdout_redirect 'log/api.log', 'log/api_err.log', true
-end
-
 bind "tcp://0.0.0.0:#{port}"
